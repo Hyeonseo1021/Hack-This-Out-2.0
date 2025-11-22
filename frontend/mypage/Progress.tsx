6a76cefb9ac09dbdea3f97c80f9e6e2b5cbf5a41
Kimeunda <ruyo22epdl@gmail.com>
2025-11-22 14:54:48 +0900

    commit

---
+++ frontend/mypage/Progress.tsx
@@ -0,0 +1,145 @@
+import { useEffect, useState } from "react";
+import { getUserProgress } from "../../api/axiosUser";
+import { getContestParticipations } from "../../api/axiosContest";
+import LoadingIcon from "../public/LoadingIcon";
+import ErrorIcon from "../public/ErrorIcon";
+import { UserProgressItem, ContestParticipationItem } from "../../types/Progress";
+import { formatDate, formatTimeSpent } from "../../utils/dateUtils";
+import { Avatar } from "@mui/material";
+import { avatarBackgroundColors } from "../../utils/avatars";
+import { getAvatarColorIndex } from "../../utils/avatars";
+import '../../assets/scss/mypage/Progress.scss';
+
+
+const Progress = () => {
+    const [userProgress, setUserProgress] = useState<UserProgressItem[]>([]);
+    const [contestParticipation, setContestParticipation] = useState<ContestParticipationItem[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const [activeTab, setActiveTab] = useState<"machine" | "contest">("machine");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const progressResponse = await getUserProgress();
+                setUserProgress(progressResponse.userProgress);
+                
+                const participationResponse = await getContestParticipations();
+                setContestParticipation(participationResponse.participations);
+            } catch (err: any) {
+                setError(err.message || "An error occurred while fetching data.");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+
+    if (loading) return <LoadingIcon />;
+    if (error) return <ErrorIcon />;
+
+    return (
+        <div className="progress-container">
+            <h2>History</h2>
+            <div className="tabs">
+                <button
+                    className={`tab-button ${activeTab === "machine" ? "active" : ""}`}
+                    onClick={() => setActiveTab("machine")}
+                >
+                    Machine History
+                </button>
+                <button
+                    className={`tab-button ${activeTab === "contest" ? "active" : ""}`}
+                    onClick={() => setActiveTab("contest")}
+                >
+                    Contest History
+                </button>
+            </div>
+
+            {activeTab === "machine" ? (
+                userProgress.length > 0 ? (
+                    <table className="progress-table">
+                        <thead>
+                            <tr className="head-detail">
+                                <th className="head-name">Machine Name</th>
+                                <th className="head-exp">EXP Earned</th>
+                                <th className="head-time">Time Spent</th>
+                                <th className="head-hint">Hints Used</th>
+                                <th className="head-comp">Completed</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {userProgress.map((progress) => {
+                                const machineName = progress.machine?.name || 'Unknown';
+                                const avatarColorIndex = getAvatarColorIndex(machineName);
+                                const avatarBgColor = avatarBackgroundColors[avatarColorIndex];
+                                return (
+                                <tr className="body-detail" key={progress._id}>
+                                    <td className="body-name">
+                                        <Avatar 
+                                        variant="rounded" 
+                                        sx={{ backgroundColor: avatarBgColor, width: 40, height: 40 }}>
+                                        {machineName.charAt(0).toUpperCase()}
+                                        </Avatar> {machineName.charAt(0).toUpperCase() + machineName.slice(1)}
+                                    </td>
+                                    <td className="body-exp">{progress.expEarned} EXP</td>
+                                    <td className="body-time">{progress.timeSpent === new Date(0) ? "-" : formatTimeSpent(new Date(progress.timeSpent))}</td>
+                                    <td className="body-hint">{progress.hintsUsed}</td>
+                                    <td className="body-comp">{progress.completedAt ? formatDate(progress.completedAt) : "Not completed"}</td>
+                                    </tr>
+                                );
+                            })}
+                        </tbody>
+                    </table>
+                ) : (
+                    <p>No machine history available.</p>
+                )
+            ) : contestParticipation.length > 0 ? (
+                <table className="contest-table">
+                    <thead>
+                        <tr className="head-detail">
+                            <th className="head-name">Contest Name</th>
+                            <th className="head-time">Start Time</th>
+                            <th className="head-comp">Completed</th>
+                            <th className="head-exp">EXP Earned</th>
+                            <th className="head-machine">Machines Completed</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {contestParticipation.map((participation) => {
+                            const contestName = participation.contest.name || 'Unknown';
+                            const avatarColorIndex = getAvatarColorIndex(contestName);
+                            const avatarBgColor = avatarBackgroundColors[avatarColorIndex];
+                            return (
+                            <tr className="body-detail" key={participation._id}>
+                                <td className="body-name"><Avatar 
+                                    variant="rounded" 
+                                    sx={{ backgroundColor: avatarBgColor, width: 40, height: 40 }}>
+                                    {contestName.charAt(0).toUpperCase()}
+                                    </Avatar> {contestName.charAt(0).toUpperCase() + contestName.slice(1)}
+                                    </td>
+                                <td className="body-time">{formatDate(participation.participationStartTime)}</td>
+                                <td className="body-comp">
+                                    {participation.participationEndTime
+                                        ? formatDate(participation.participationEndTime)
+                                        : "On going"}
+                                </td>
+                                <td className="body-exp">{participation.expEarned === 0 ? "Given up" : `${participation.expEarned} EXP`}</td>
+                                <td className="body-machine">
+                                    {participation.machineCompleted.length}
+                                </td>
+                            </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+            ) : (
+                <p>No contest history available.</p>
+            )}
+        </div>
+    );
+};
+
+export default Progress;
---
+++ frontend/mypage/UserStats.tsx
@@ -0,0 +1,148 @@
+import { useEffect, useState } from "react";
+import { getMyRank } from "../../api/axiosUser";
+import { CurrentUser } from "../../types/CurrentUser";
+import ErrorIcon from "../public/ErrorIcon";
+import LoadingIcon from "../public/LoadingIcon";
+import { Avatar, Badge, Box, Typography, CircularProgress } from "@mui/material";
+import { avatarBackgroundColors, getAvatarColorIndex } from "../../utils/avatars";
+import { styled } from "@mui/material/styles";
+import '../../assets/scss/mypage/userStats.scss';
+import { getLevelFrame, getRankBadge, levelFrames, rankBadges } from "../../utils/badge";
+
+// Define the props for StyledBadge to include the frame number
+interface StyledBadgeProps {
+    frame: number;
+}
+
+// StyledBadge component that dynamically changes the badge frame based on the `frame` prop
+const StyledBadge = styled(Badge)<StyledBadgeProps>(() => ({
+    '& .MuiBadge-badge': {
+        backgroundColor: 'transparent',
+        padding: 0, // Remove default padding
+        minHeight: 'auto',
+        minWidth: 'auto',
+        borderRadius: '50%', // Ensure the badge is circular
+        // Adjust the size of the badge as needed
+        width: 230,
+        height: 230,
+        position: 'absolute',
+        top: -155, // ****************!! Adjust this value to position the badge !!***************
+        left: -155, // ****************!! Adjust this value to position the badge !!***************
+        '& img': {
+            width: '100%',
+            height: '100%',
+            objectFit: 'contain',
+        },
+    },
+}));
+
+const UserStats = () => {
+    const [myStats, setMyStats] = useState<CurrentUser | null>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+                const response = await getMyRank();
+                setMyStats({
+                    _id: response.user._id,
+                    myRank: response.myRank,
+                    myLevel: response.user.level,
+                    myExp: response.user.exp,
+                    myUsername: response.user.username,
+                    myAvatar: response.user.avatar,
+                });
+            } catch (err: any) {
+                setError(err.message || "An error occurred while fetching data.");
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchData();
+    }, []);
+
+    if (loading) return <LoadingIcon />;
+    if (error) return <ErrorIcon />;
+
+    if (!myStats) {
+        return <Box>No data available.</Box>;
+    }
+
+    const avatarBgColor = avatarBackgroundColors[getAvatarColorIndex(myStats.myUsername || '')];
+
+    // Helper function to determine the experience needed for the next level
+    const getNextLevelExp = (currentLevel: number): number => {
+        const levels = [
+            0, 100, 300, 600, 1000, 1500, 2100, 3000, 4000, 5000,
+            6000, 7000, 8000, 9000, 10000, 11000, 12000, 13000, 14000, 15000
+        ]; // Must match backend thresholds
+        if (currentLevel < 1) return levels[1];
+        if (currentLevel >= levels.length) return levels[levels.length - 1]; // Max level
+        return levels[currentLevel];
+    };
+
+    const levelFrame = getLevelFrame(myStats.myLevel || 1);
+    const nextLevelExp = getNextLevelExp(myStats.myLevel || 1);
+    const expPercentage = ((myStats?.myExp || 0) / nextLevelExp) * 100;
+    const remainExp = nextLevelExp - (myStats?.myExp || 0);
+    const rankBadge = getRankBadge(myStats.myRank || 0);
+
+    return (
+        <Box className="stats-container">
+            <Typography className="stats-header">{myStats?.myUsername}'s Stats</Typography>
+            <Box className="stats-upper-container">
+                <Box className="stats-avatar">
+                    <StyledBadge
+                        overlap="circular"
+                        anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                        variant="standard"
+                        frame={levelFrame}
+                        badgeContent={<img src={levelFrames[levelFrame]} alt="Level Frame" />}
+                    >
+                        <Avatar
+                            alt={myStats?.myUsername || ''}
+                            className="stats-avatar-image"
+                            sx={{
+                                backgroundColor: avatarBgColor
+                            }}
+                        >
+                            {myStats?.myUsername?.charAt(0).toUpperCase() || ''}
+                        </Avatar>
+                    </StyledBadge>
+                </Box>
+                <Box className="stats-progress">
+                    <Box className="stats-progress-container">
+                        <CircularProgress
+                            variant="determinate"
+                            value={Math.min(expPercentage, 100)}
+                            size={200}
+                            thickness={4}
+                            className="progress-bar"
+                            aria-label={`Level ${myStats?.myLevel}, ${Math.round(expPercentage)}% to next level`}
+                        />
+                        <Box className="progress-text">
+                            <Typography style={{fontSize: "1.5rem", fontWeight: "bold"}} component="div" color="var(--color-white)">
+                                LVL. {myStats?.myLevel}
+                            </Typography>
+                            <Typography variant="h6" component="div" color="var(--color-white)">
+                                {Math.round(expPercentage)}%
+                            </Typography>
+                        </Box>
+                    </Box>
+                    <Typography className="next-level" variant="h6" component="div">Next Level: {remainExp} EXP</Typography>
+                </Box>
+                <Box className="stats-rank">
+                    <Avatar
+                        src={rankBadges[rankBadge]}
+                        className="rank-badge"
+                    />
+                    <Typography variant="h6">Ranking #{myStats?.myRank}</Typography>
+                </Box>
+            </Box>
+        </Box>
+    );
+};
+
+export default UserStats;
